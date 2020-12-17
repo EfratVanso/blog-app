234c7c96e5cd84f16f712ed448c30b0ac15b13ce
efiVanso <efi.vanso@gmail.com>
2020-12-17 19:54:25 +0200

    Rendering a List of Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const BlogContext = React.createContext();
 
 //provider wrap the whole app
-export const BlogProvider = ({children}) => {
-    // value is the information to share:
-    return <BlogContext.Provider value={5}> 
-    {/* this will be other component(actually App.js) */}
-      {children}  
-    </BlogContext.Provider>;
+export const BlogProvider = ({ children }) => {
+    const blogPosts = [
+        {title: 'Blog Post #1'},
+        {title: 'Blog Post #2'},
+    ];
+
+
+  // value is the information to share:
+  return (
+    <BlogContext.Provider value={blogPosts}>
+      {/* this will be other component(actually App.js) */}
+      {children}
+    </BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,12 +1,19 @@
 import React, {useContext} from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 export default function IndexScreen() {
-    const value = useContext(BlogContext);
+    const blogPosts = useContext(BlogContext);
     return (
         <View>
-            <Text>{value}</Text>
+            <Text>IndexScreen</Text>
+            <FlatList 
+            data={ blogPosts}
+            keyExtractor={(post) =>post.title}
+            renderItem={ ({item}) => {
+                return <Text>{item.title}</Text>
+            }}
+            />
         </View>
     )
 }
