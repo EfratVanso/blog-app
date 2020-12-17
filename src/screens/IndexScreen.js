95630c2ab5ce31070861f31a4de9af6b9f01ae15
efiVanso <efi.vanso@gmail.com>
2020-12-17 18:57:46 +0200

    add react navigation

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function IndexScreen() {
+    return (
+        <View>
+            <Text>jhgjh</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
