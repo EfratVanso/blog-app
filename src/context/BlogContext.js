d6d12c4c6ccfdaff0221c6e505a12ccfe450d194
efiVanso <efi.vanso@gmail.com>
2020-12-19 20:08:48 +0200

    Adding State in Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const BlogContext = React.createContext();
 
 //provider wrap the whole app
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        {title: 'Blog Post #1'},
-        {title: 'Blog Post #2'},
-    ];
+    // const blogPosts = [
+    //     {title: 'Blog Post #1'},
+    //     {title: 'Blog Post #2'},
+    // ];
+    const [blogPosts, setBlogPosts]= useState([]);
 
-
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length+1}`}]);
+    };
   // value is the information to share:
   return (
-    <BlogContext.Provider value={blogPosts}>
+    <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
       {/* this will be other component(actually App.js) */}
       {children}
     </BlogContext.Provider>
