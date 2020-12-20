4bf9805d640c286c63e5e3ec688c2ece15423fdb
efiVanso <efi.vanso@gmail.com>
2020-12-20 23:05:31 +0200

    Deleting Post
    Updating the Reducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react'
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
 import {Context} from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons';
 
 export default function IndexScreen() {
-    const {state, addBlogPost} = useContext(Context);
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
     return (
         <View>
             <Button title="Add Post" onPress={addBlogPost}/>
@@ -13,7 +13,9 @@ export default function IndexScreen() {
             keyExtractor={(post) =>post.title}
             renderItem={ ({item}) => {
                 return <View style={styles.row}>
+                    <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
                     <Feather name="trash" size={24} color="black" style={styles.icon} />
+                    </TouchableOpacity>
                     <Text style={styles.title}>{item.title}</Text></View> 
             }}
             />
