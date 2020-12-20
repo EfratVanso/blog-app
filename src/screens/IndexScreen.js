7a491fc6ae758dd43044fba8647320422b24c641
efiVanso <efi.vanso@gmail.com>
2020-12-20 21:56:18 +0200

    Adding delete icon and some style to the list

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,20 @@
 import React, {useContext} from 'react'
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import {Context} from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons';
 
 export default function IndexScreen() {
     const {state, addBlogPost} = useContext(Context);
     return (
         <View>
-            <Text>IndexScreen</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList 
             data={ state}
             keyExtractor={(post) =>post.title}
             renderItem={ ({item}) => {
-                return <Text>{item.title}</Text>
+                return <View style={styles.row}>
+                    <Feather name="trash" size={24} color="black" style={styles.icon} />
+                    <Text style={styles.title}>{item.title}</Text></View> 
             }}
             />
         </View>
@@ -20,5 +22,18 @@ export default function IndexScreen() {
 }
 
 const styles = StyleSheet.create({
-    
+    row:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:15,
+        paddingHorizontal:10,
+        borderTopWidth:1,
+        borderColor:'gray'
+    },
+    title:{
+        fontSize:18
+    },
+    icon:{
+        fontSize:24
+    }
 })
