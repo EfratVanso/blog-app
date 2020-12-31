ff40ddb560f585a2312f6e01bdcb73cc665c7891
efiVanso <efi.vanso@gmail.com>
2020-12-31 17:06:02 +0200

    Add The Edit Icon Link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,20 +1,27 @@
-import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Context } from '../context/BlogContext'
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
-export default function ShowScreen({navigation}) {
-    const {state} = useContext(Context);// get the list of blogs
-    const blogPost = state.find(
-        blogPost => blogPost.id === navigation.getParam('id')
-    );
-    return (
-        <View> 
-            <Text>{blogPost.title}</Text>
-        </View>
-    )
+export default function ShowScreen({ navigation }) {
+  const { state } = useContext(Context); // get the list of blogs
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
+    </View>
+  );
+}
+ShowScreen.navigationOptions = () => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
-
-const styles = StyleSheet.create({
-    
-})
-
+const styles = StyleSheet.create({});
