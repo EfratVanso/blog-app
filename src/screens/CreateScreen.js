5290dac89055ad2f252cf3e3651be9e39e0b7354
efiVanso <efi.vanso@gmail.com>
2020-12-31 12:56:56 +0200

    Adding a Creation Screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext';
+
+export default function CreateScreen() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
