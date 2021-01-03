0dc2d5fc02369fdc952d117384fc8cfb24d22996
efiVanso <efi.vanso@gmail.com>
2021-01-03 02:53:12 +0200

    Deleting a Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -8,15 +8,6 @@ const blogReducer = (state, action) => {
     case "get_blogposts":
       return action.payload;
 
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        },
-      ];
     case "delete_blogpost":
         return state.filter((blogPost)=> blogPost.id !== action.payload);
     case "edit_blogpost": 
@@ -39,12 +30,12 @@ return async () => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', {title, content});
-    // dispatch({ type: "add_blogpost" , payload:{title, content}});
      callback();
   };
 };
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload:id });
   };
 };
